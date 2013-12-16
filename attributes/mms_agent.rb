5e759ab567bfc877e8f40b7c2dcb0bcd5dda7369
Richard Henning <rhenning@neat.com>
2013-12-16 17:18:54 -0500

    Download MMS agent from new location

--- attributes/mms_agent.rb
+++ attributes/mms_agent.rb
@@ -1,5 +1,6 @@
 default[:mongodb][:mms_agent][:api_key] = ""
 default[:mongodb][:mms_agent][:secret_key] = ""
 
+default[:mongodb][:mms_agent][:source_uri] = "https://mms.mongodb.com/settings/mms-monitoring-agent.zip"
 default[:mongodb][:mms_agent][:install_dir] = "/usr/local/share"
 default[:mongodb][:mms_agent][:log_dir] = "#{node[:mongodb][:logpath]}/agent"
