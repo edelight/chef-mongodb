1526bb971c185570632ec3560f3b6a68a4be7d11
Chris Coppick <chris.coppick@tealium.com>
2014-09-14 20:15:03 -0500

    Refactor raid creation out of shard recipe.

--- recipes/shard.rb
+++ recipes/shard.rb
@@ -19,60 +19,7 @@
 # limitations under the License.
 #
 
-if node[:cloud] and 
-   node[:cloud][:provider] == 'ec2' and
-   node[:mongodb][:raid] then
-
-   include_recipe 'aws'
-
-   if node[:mongodb][:raid_ebs_type] == 'io1' then
-      aws_ebs_raid "data_raid" do
-	 level node[:mongodb][:raid_level]
-	 disk_count node[:mongodb][:raid_disk_count]
-	 disk_size node[:mongodb][:raid_disk_size]
-	 disk_type node[:mongodb][:raid_ebs_type]
-	 disk_piops node[:mongodb][:raid_ebs_piops]
-	 mount_point node[:mongodb][:raid_mount]
-	 snapshots node[:mongodb][:raid_snaps]
-	 filesystem node[:mongodb][:raid_fs]
-	 filesystem_options node[:mongodb][:raid_fs_opts]
-	 action [ :auto_attach ]
-      end
-   else
-      aws_ebs_raid "data_raid" do
-	 level node[:mongodb][:raid_level]
-	 disk_count node[:mongodb][:raid_disk_count]
-	 disk_size node[:mongodb][:raid_disk_size]
-	 disk_type node[:mongodb][:raid_ebs_type]
-	 mount_point node[:mongodb][:raid_mount]
-	 snapshots node[:mongodb][:raid_snaps]
-	 filesystem node[:mongodb][:raid_fs]
-	 filesystem_options node[:mongodb][:raid_fs_opts]
-	 action [ :auto_attach ]
-      end
-   end
-
-   # setup raid mount in fstab
-   Chef::Log.info("Adding mount info to fstab for raid device.")
-   mount node[:mongodb][:raid_mount] do
-      device lazy { 
-	 "/dev/#{node[:aws][:raid][node[:mongodb][:raid_mount]][:raid_dev]}"
-      }
-      fstype node[:mongodb][:raid_fs]
-      options node[:mongodb][:raid_fs_opts]
-      action [:enable]
-   end
-
-   if node[:mongodb][:encfs] then
-      include_recipe 'tealium_encfs'
-
-      tealium_encfs_mount node[:mongodb][:data_root] do
-         encrypted_data node[:mongodb][:raid_mount]
-	 fuse_opts 'noatime'
-      end
-   end
-
-end
+include_recipe "mongodb::raid"
 
 cookbook_file '/usr/local/bin/mongo_compact.rb' do
    source 'mongo_compact.rb'
