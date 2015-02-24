f8ea37c24eb31fb1d743b48ec8edf3b0cb6e93f9
Timor Tsentsiper <timor@codecademy.com>
2015-02-24 16:37:17 -0500

    use build-essential

--- recipes/mongo_gem.rb
+++ recipes/mongo_gem.rb
@@ -1,16 +1,13 @@
 # The build-essential cookbook was not running during the compile phase, install gcc explicitly for rhel so native
 # extensions can be installed
-gcc = package 'gcc' do
-  action :nothing
-  only_if { platform_family?('rhel') }
-end
-gcc.run_action(:install)
+
+include_recipe 'build-essential'
 
 if platform_family?('rhel')
   sasldev_pkg = 'cyrus-sasl-devel'
 else
   sasldev_pkg = 'libsasl2-dev'
-end 
+end
 
 package sasldev_pkg do
   action :nothing
