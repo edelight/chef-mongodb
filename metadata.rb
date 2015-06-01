a4451a3f4932968d0ee079fc192ae46ed2dd9584
Chris Coppick <chris.coppick@tealium.com>
2015-06-01 16:18:17 -0500

    Changed tcp_keepalive from 300 to 120 per new mongodb.com recommendation.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Tealium"
 maintainer_email  "devops@tealium.com"
 license           "Apache 2.0"
 description       "Installs and configures mongodb"
-version           "0.15.2"
+version           "0.15.3"
 
 recipe "mongodb", "Installs and configures a single node mongodb instance"
 recipe "mongodb::10gen_repo", "Adds the 10gen repo to get the latest packages"
